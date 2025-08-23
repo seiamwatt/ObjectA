153850bdd2fc7e3bd21abbae54f97c00fd5309e1
Vector <eiamw001@umn.edu>
2025-08-23 19:04:08 +0700

    add experimental python script

---
+++ test.py
@@ -0,0 +1,29 @@
+# console test 
+
+import csv
+import statistics
+import pandas as pd
+import argparse
+import os
+import glob
+
+
+def main():
+    parser = argparse.ArgumentParser(description="experimental skip tracer")
+    parser.add_argument("--input",type = str,required=True)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
